97f6e9217e1018085d6a0861e0899283e5d9ff8b
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-04-16 17:08:06 +0700

    Создан проект

---
+++ src/main/java/com/qwerty/codehedgehog/CodeHedgehogApplication.java
@@ -0,0 +1,13 @@
+package com.qwerty.codehedgehog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeHedgehogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeHedgehogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qwerty/codehedgehog/CodeHedgehogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwerty.codehedgehog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeHedgehogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
