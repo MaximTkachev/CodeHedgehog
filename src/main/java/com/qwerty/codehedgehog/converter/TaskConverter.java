eb5e25fef77bf98e6333fa06b4ca11d3e616f3e7
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-04-18 20:15:55 +0700

    Написан конвертер для dto для запросов на задания

---
+++ src/main/java/com/qwerty/codehedgehog/converter/TaskConverter.java
@@ -0,0 +1,38 @@
+package com.qwerty.codehedgehog.converter;
+
+import com.qwerty.codehedgehog.dto.Task.CreateTaskDto;
+import com.qwerty.codehedgehog.dto.Task.TaskDto;
+import com.qwerty.codehedgehog.entity.TaskEntity;
+import com.qwerty.codehedgehog.entity.TopicEntity;
+
+import java.util.UUID;
+
+public class TaskConverter {
+    public static TaskDto convertEntityToDto(TaskEntity taskEntity) {
+        String topicId;
+        if (taskEntity.getTopic() != null) {
+            topicId = taskEntity.getTopic().getId();
+        }
+        else
+            topicId = null;
+
+        return new TaskDto(
+                taskEntity.getId(),
+                taskEntity.getName(),
+                topicId,
+                taskEntity.getDescription(),
+                taskEntity.getPrice()
+        );
+    }
+
+    public static TaskEntity convertCreateDtoToEntity(CreateTaskDto createTaskDto, TopicEntity topic) {
+        return new TaskEntity(
+                UUID.randomUUID().toString(),
+                createTaskDto.getName(),
+                topic,
+                createTaskDto.getDescription(),
+                createTaskDto.getPrice(),
+                false
+        );
+    }
+}
