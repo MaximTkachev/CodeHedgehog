0337677ab59911b15d7542a5f1368bd1251633df
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-04-19 16:20:36 +0700

    Добавлен метод поиска примеров по задаче

--- src/main/java/com/qwerty/codehedgehog/repository/SampleRepository.java
+++ src/main/java/com/qwerty/codehedgehog/repository/SampleRepository.java
@@ -1,7 +1,12 @@
 package com.qwerty.codehedgehog.repository;
 
 import com.qwerty.codehedgehog.entity.SampleSolutionEntity;
+import com.qwerty.codehedgehog.entity.TaskEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SampleRepository extends CrudRepository<SampleSolutionEntity, String> {
+
+    List<SampleSolutionEntity> getAllByTask(TaskEntity task);
 }
